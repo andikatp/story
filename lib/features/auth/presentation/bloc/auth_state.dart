7d479166574c1c486a6f23b47b0477db8b9afd73
andikatp <triprasetya_andika@yahoo.com>
2024-02-08 20:50:31 +0700

    bloc ready

--- lib/features/auth/presentation/bloc/auth_state.dart
+++ lib/features/auth/presentation/bloc/auth_state.dart
@@ -2,9 +2,39 @@ part of 'auth_bloc.dart';
 
 sealed class AuthState extends Equatable {
   const AuthState();
-  
+
   @override
   List<Object> get props => [];
 }
 
-final class AuthInitial extends AuthState {}
+final class AuthInitial extends AuthState {
+  const AuthInitial();
+}
+
+final class AuthLoading extends AuthState {
+  const AuthLoading();
+}
+
+final class LoggedIn extends AuthState {
+  const LoggedIn({required this.user});
+  final UserEntity user;
+
+  @override
+  List<Object> get props => [user];
+}
+
+final class Registered extends AuthState {
+  const Registered();
+}
+
+final class TokenSaved extends AuthState {
+  const TokenSaved();
+}
+
+final class AuthError extends AuthState {
+  const AuthError({required this.message});
+  final String message;
+
+  @override
+  List<String> get props => [message];
+}
