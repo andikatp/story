6ce4a3e2dfe0b000522784822e5f28af7546eb63
andikatp <triprasetya_andika@yahoo.com>
2024-02-10 00:40:28 +0700

    button theme

--- lib/core/res/colours.dart
+++ lib/core/res/colours.dart
@@ -4,6 +4,6 @@ class Colours {
   Colours._();
   static const primaryColor = Color(0xFF6ca7fb);
   static const whiteColor = Color(0xFFf5f5f5);
-  static const backgroundColor = Color(0x00121922);
+  static const backgroundColor = Color(0xFF10161E);
   static const secondaryColor = Color(0xFF3b68ea);
 }
