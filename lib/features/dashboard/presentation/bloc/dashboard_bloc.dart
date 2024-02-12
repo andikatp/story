1305e6f9ec0a796104e96e0acd9c1ae9e72ed8a0
andikatp <triprasetya_andika@yahoo.com>
2024-02-12 12:42:28 +0700

    infinite story

--- lib/features/dashboard/presentation/bloc/dashboard_bloc.dart
+++ lib/features/dashboard/presentation/bloc/dashboard_bloc.dart
@@ -1,6 +1,5 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:story/core/errors/error_message.dart';
 import 'package:story/features/dashboard/domain/entities/story_entity.dart';
 import 'package:story/features/dashboard/domain/usecases/get_stories.dart';
 
@@ -11,23 +10,36 @@ class DashboardBloc extends Bloc<DashboardEvent, DashboardState> {
   DashboardBloc({
     required GetStories getStories,
   })  : _getStories = getStories,
-        super(const DashboardInitial()) {
-    on<DashboardEvent>((event, emit) {
-      emit(const DashboardLoading());
+        super(const DashboardState()) {
+    on<DashboardEvent>((event, emit) async {
+      if (event is DashboardGetStories && !state.hasReachedMax) {
+        final result = await _getStories(event.page);
+        result.fold(
+          (failure) {
+            emit(
+              state.copyWith(
+                status: StoryStatus.error,
+                errorMessage: failure.errorMessage,
+              ),
+            );
+          },
+          (stories) {
+            final newStories =
+                stories.isEmpty ? <StoryEntity>[] : List.of(state.stories)
+                  ..addAll(stories);
+            final hasReachedMax = stories.isEmpty;
+            emit(
+              state.copyWith(
+                status: StoryStatus.success,
+                stories: newStories,
+                hasReachedMax: hasReachedMax,
+              ),
+            );
+          },
+        );
+      }
     });
-    on<DashboardGetStories>(getStoriesEvent);
   }
 
   final GetStories _getStories;
-
-  Future<void> getStoriesEvent(
-    DashboardGetStories event,
-    Emitter<DashboardState> emit,
-  ) async {
-    final result = await _getStories(event.page);
-    result.fold(
-      (failure) => emit(DashboardError(message: errorMessage(failure))),
-      (stories) => emit(DashboardLoaded(stories: stories)),
-    );
-  }
 }
---
+++ lib/features/dashboard/presentation/widgets/tile.dart
@@ -0,0 +1,99 @@
+import 'dart:ui';
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:story/core/extensions/extension.dart';
+import 'package:story/core/utils/time_utils.dart';
+import 'package:story/features/dashboard/domain/entities/story_entity.dart';
+
+class Tile extends StatelessWidget {
+  const Tile({required this.story, super.key});
+
+  final StoryEntity story;
+
+  @override
+  Widget build(BuildContext context) {
+    Future<dynamic> showDetail() {
+      return showDialog(
+        context: context,
+        builder: (context) {
+          return BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 1.5, sigmaY: 1.5),
+            child: AlertDialog(
+              contentPadding: EdgeInsets.zero,
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 60.h,
+                    padding: REdgeInsets.symmetric(horizontal: 20),
+                    child: Wrap(
+                      spacing: 4,
+                      runAlignment: WrapAlignment.center,
+                      crossAxisAlignment: WrapCrossAlignment.center,
+                      children: [
+                        Text(
+                          story.name,
+                          style: context.bodyMedium,
+                        ),
+                        const Icon(Icons.circle, size: 4),
+                        Text(
+                          TimeUtils.formatTimeDifference(story.createdAt),
+                          style: context.bodySmall
+                              .copyWith(fontSize: 12, color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                  ),
+                  CachedNetworkImage(
+                    key: key,
+                    imageUrl: story.photoUrl,
+                    fit: BoxFit.cover,
+                    height: 300.h,
+                    width: 400.w,
+                    placeholder: (_, __) => const Center(
+                      child: CupertinoActivityIndicator(),
+                    ),
+                    errorWidget: (_, __, ___) => const Icon(Icons.error),
+                  ),
+                  Container(
+                    padding:
+                        REdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                    alignment: Alignment.center,
+                    child: Text(story.description),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      );
+    }
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16).r,
+      child: GestureDetector(
+        onTap: showDetail,
+        child: CachedNetworkImage(
+          key: key,
+          imageUrl: story.photoUrl,
+          fit: BoxFit.cover,
+          placeholder: (_, __) => const Center(
+            child: CupertinoActivityIndicator(),
+          ),
+          errorWidget: (_, __, ___) => const Icon(Icons.error),
+          cacheManager: CacheManager(
+            Config(
+              'story',
+              stalePeriod: const Duration(minutes: 5),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
