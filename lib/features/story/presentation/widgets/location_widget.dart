2590290b368b970dbba12c3f8776556599ef016e
andikatp <triprasetya_andika@yahoo.com>
2024-04-22 09:47:37 +0700

    fix bug

--- lib/features/story/presentation/widgets/location_widget.dart
+++ lib/features/story/presentation/widgets/location_widget.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:another_flushbar/flushbar.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
